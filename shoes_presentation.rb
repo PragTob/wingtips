6e6b5676676f1c057a5cb472aadc1fffbc34b493
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-12 00:39:15 +0200

    general shoes information

--- shoes_presentation.rb
+++ shoes_presentation.rb
@@ -2,7 +2,7 @@ require_relative 'slide'
 Dir.glob('slides/*').each do |slide_file|
   require_relative slide_file
 end
-SLIDES = [Slide, SqueakSlide, CodeSlide, MetaSlide, MissingBeforeAlpha, Outlook, PhotoCredit]
+SLIDES = [Slide, SqueakSlide, General, MetaSlide, MissingBeforeAlpha, Outlook, PhotoCredit]
 CODE_SIZE = 30
 BULLET_POINT_SIZE = 40
 HEADLINE_SIZE = 65
---
+++ slides/general.rb
@@ -0,0 +1,10 @@
+class General < Slide
+  def content
+    headline 'What is Shoes?'
+    bullet 'GUI toolkit'
+    bullet 'DSL'
+    bullet 'cross-platform'
+    bullet 'packaging'
+    bullet 'simple'
+  end
+end
