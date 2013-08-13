3520822ec282a1cdf963fc871bf6c69d91d22b2c
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-13 16:03:57 +0200

    Little adjustments

--- slides/shoes4.rb
+++ slides/shoes4.rb
@@ -2,8 +2,8 @@ class Shoes4 < Slide
   def content
     headline 'Shoes4'
     bullet 'unite community on one implementation'
-    bullet 'complete rewrite'
-    bullet 'shooting for 100% compatibility'
+    bullet 'complete all ruby rewrite'
+    bullet 'aiming for 100% compatibility'
     bullet 'pluggable backend'
     bullet 'default backend: JRuby + SWT'
   end
