ae4ab0bba4c65272fc0271318fbddbf504acf9ab
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-13 16:02:27 +0200

    Modigied squeak image and added power point image

--- slides/libreoffice.rb
+++ slides/libreoffice.rb
@@ -3,3 +3,9 @@ class Libreoffice < Slide
     fullscreen_image 'images/libreoffice.png'
   end
 end
+
+class PowerPoint < Slide
+  def content
+    fullscreen_image 'images/powerpoint.png'
+  end
+end
