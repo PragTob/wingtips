71f287b113975d6f4e38434f8c92ac7e6983eb56
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-08-13 17:14:43 +0200

    presenters next and before keys

--- shoes_presentation.rb
+++ shoes_presentation.rb
@@ -4,8 +4,8 @@ Dir.glob('slides/*').each do |slide_file|
   require_relative slide_file
 end
 
-NEXT_KEYS = [:right]
-PREVIOUS_KEYS = [:left]
+NEXT_KEYS = [:right, :page_down]
+PREVIOUS_KEYS = [:left, :page_up]
 
 SLIDES = [Slide, PluggableBackend, Squeak, Keynote, PowerPoint, Libreoffice, IHeartShoes, Title, General, Shoes4, PluggableBackend, Samples, MoreSamples, BasicApp, HowDoesThisWork, MissingBeforeAlpha, BeforeRelease, LeftOut, WhyCare, Web, Console, Learn, Tool, GradeClass, Fun, IHeartShoes, ShoesOn, PhotoCredit]
 CODE_SIZE = 30
@@ -13,6 +13,5 @@ BULLET_POINT_SIZE = 40
 HEADLINE_SIZE = 65
 VERY_BIG_SIZE = 80
 ENORMOUS_SIZE = 140
-
 
 Presentation.start
