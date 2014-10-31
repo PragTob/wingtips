f1221a742e2586c21cd3c40661c1ff87d3e1d44e
Jason R. Clark <jclark@newrelic.com>
2014-10-31 14:51:46 -0700

    Simpler way to define slides

--- lib/wingtips/configuration.rb
+++ lib/wingtips/configuration.rb
@@ -6,16 +6,31 @@ module Wingtips
       path = File.expand_path(path)
       dir  = File.dirname(path)
       Dir[File.join(dir, "slides/*.rb")].each do |file|
-        require file unless file == path
+        self.instance_eval(File.read(file)) unless file == path
       end
 
+      @slide_classes = [Wingtips::Slide]
+
       self.instance_eval(File.read(path))
+    end
+
+    def slide(title=nil, &content)
+      clazz = Class.new(Wingtips::Slide)
+      clazz.class_eval do
+        define_method(:content, &content)
+      end
+
+      if title.nil?
+        @slide_classes << clazz
+      else
+        Object.const_set(title, clazz)
+      end
     end
 
     def slides(*slide_classes)
       # the empty slide at the start is needed as otherwise the dimensions
       # of the first slide are most likely messed up
-      @slide_classes = [Wingtips::Slide] + slide_classes
+      @slide_classes.concat(slide_classes)
     end
   end
 end
