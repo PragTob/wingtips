0da93e618dd6490ed10a6b22e482cec0d7108177
Tobias Pfeiffer <pragtob@gmail.com>
2015-04-18 12:56:12 +0200

    Load options first, fail, then load slides and load again
    * see comment in configuration in this commit for a more throrough
      explanation of this approach

--- lib/wingtips/configuration.rb
+++ lib/wingtips/configuration.rb
@@ -10,10 +10,13 @@ module Wingtips
         app: {
           title:      'Presentation',
           fullscreen: true
-        }
+        },
+        templates: {}
       }
 
+
       full_path = File.expand_path(path)
+      load_options(full_path)
       load_templates full_path
       load_named_slides full_path
 
@@ -22,6 +25,27 @@ module Wingtips
       @slide_classes = [Wingtips::Slide]
 
       self.instance_eval(File.read(full_path))
+    end
+
+    def load_options(full_path)
+      begin
+        @allow_unnamed_slides = true
+        self.instance_eval(File.read(full_path))
+      rescue NameError
+        @allow_unnamed_slides = false
+        # expected to hit the first slide class and then abort evaling
+        # the config file. This is necessary so we can have the options at the
+        # beginning of the config file while they are needed in the tamplates
+        # for instance. So what happens is:
+        # 1. read config (options should be on top)
+        # 2. error upon encountering the first class, continue in initialize
+        # 3. Load slides and templates (while options are already loaded)
+        # 4. eval the config file again (options are read again, shouldn't be
+        #   a problem). This time no error as slide and template classes are
+        #   loaded
+        # Yes there could be a separate options file, but it didn't feel right
+        # to have a config and an options file
+      end
     end
 
     def app_options
